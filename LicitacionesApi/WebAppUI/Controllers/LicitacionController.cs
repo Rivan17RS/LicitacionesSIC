f028ffed6ca856fbe722d647ff0b25c941640872
irojass <irojass@ucenfotec.ac.cr>
2023-03-25 09:03:31 -0600

    LicitacionesUI-Base

---
+++ LicitacionesApi/WebAppUI/Controllers/LicitacionController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppUI.Controllers
+{
+    public class LicitacionController : Controller
+    {
+        // GET: Licitacion
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Licitaciones() 
+        { 
+            return View();
+        }
+    }
+}
