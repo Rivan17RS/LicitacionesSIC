e19b9fd7f8e070e929f27523d83f388127bfe4e9
Denis Salazar <dsalazarc@ucenfotec.ac.cr>
2023-03-24 19:34:11 -0600

    DetalleLicitaciones estructura

---
+++ LicitacionesApi/DataAccess/CRUD/DetalleLicitacionesCrudFactory.cs
@@ -0,0 +1,42 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.CRUD
+{
+    public class DetalleLicitacionesCrudFactory : CrudFactory
+    {
+        public override void Create(BaseEntity entityDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Delete(BaseEntity entityDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<T> RetrieveAll<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override T RetrieveByID<T>(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override T RetrieveByIdent<T>(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(BaseEntity entityDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LicitacionesApi/DataAccess/MAPPER/DetalleLicitacionesMapper.cs
@@ -0,0 +1,48 @@
+﻿using DataAccess.DAO;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.MAPPER
+{
+    public class DetalleLicitacionesMapper : ICrudStatements, IObjectMapper
+    {
+        public BaseEntity BuildObject(Dictionary<string, object> row)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BaseEntity> BuildObjects(List<Dictionary<string, object>> lstRows)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SqlOperation GetCreateStatement(BaseEntity entyDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SqlOperation GetDeleteStatement(BaseEntity entyDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SqlOperation GetRetrieveAllStatement()
+        {
+            throw new NotImplementedException();
+        }
+
+        public SqlOperation GetRetrieveByIDStatement(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SqlOperation GetUpdateStatement(BaseEntity entyDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
