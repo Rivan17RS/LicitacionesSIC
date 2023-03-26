fd646e7c62f7b215a9bda435d8847ce507cd8ce8
irojass <irojass@ucenfotec.ac.cr>
2023-03-25 21:52:39 -0600

    Devaces_db

--- LicitacionesApi/WebAppUI/Scripts/View/Licitaciones.js
+++ LicitacionesApi/WebAppUI/Scripts/View/Licitaciones.js
@@ -1,1 +1,26 @@
-﻿
+﻿function Licitaciones() {
+
+    this.initView = function () {
+
+        this.LoadLicitacionesTable();
+    }
+
+    this.LoadLicitacionesTable() {
+        var arrayColumns = [];
+        arrayColumns[0] = { 'data': 'IdAnalista' };
+        arrayColumns[1] = { 'data': 'Titulo' };
+        arrayColumns[2] = { 'data': 'Descripcion' };
+        arrayColumns[3] = { 'data': 'LugarEntrega' };
+        arrayColumns[4] = { 'data': 'FechaCierreOfertas' };
+        arrayColumns[5] = { 'data': 'MontoPresupuestado' };
+        arrayColumns[6] = { 'data': 'Estado' };
+        arrayColumns[7] = { 'data': 'DetalleLicitaciones.IdLicitacion' };
+        arrayColumns[8] = { 'data': 'DetalleLicitaciones.Idproducto' };
+        arrayColumns[9] = { 'data': 'DetalleLicitaciones.Cantidad' };
+
+        alert('cargando tabla');
+         
+    }
+
+
+}
