a824a1a802eab975897e04cf78f5b940eed65e8b
Dns2690 <107601365+Dns2690@users.noreply.github.com>
2023-04-02 14:59:48 -0600

    Inventario pruebas

---
+++ LicitacionesApi/WebAppUI/Scripts/View/DataTesting.js
@@ -0,0 +1,102 @@
+﻿var Products = [
+    {
+        "Id": 1,
+        "Nombre": "Leche(l)",
+        "Descripcion": "Leche entera pasteurizada enriquecida con vitaminas",
+        "Precio": 2.99,
+        "FechaRegistro": "2023-04-01T18:35:00.000Z",
+        "Stock_Cantidad": 100,
+        "IdUsrCreacion": 1,
+        "FechaCreacion": "2023-04-01T18:35:00.000Z"
+    },
+    {
+        "Id": 2,
+        "Nombre": "Queso(kg)",
+        "Descripcion": "Queso semiduro de leche de vaca",
+        "Precio": 4.50,
+        "FechaRegistro": "2023-04-01T18:35:00.000Z",
+        "Stock_Cantidad": 50,
+        "IdUsrCreacion": 1,
+        "FechaCreacion": "2023-04-01T18:35:00.000Z"
+    },
+    {
+        "Id": 3,
+        "Nombre": "Yogurt(l)",
+        "Descripcion": "Yogurt natural bajo en grasas",
+        "Precio": 1.75,
+        "FechaRegistro": "2023-04-01T18:35:00.000Z",
+        "Stock_Cantidad": 200,
+        "IdUsrCreacion": 1,
+        "FechaCreacion": "2023-04-01T18:35:00.000Z"
+    },
+    {
+        "Id": 4,
+        "Nombre": "Carne de res(kg)",
+        "Descripcion": "Carne de res fresca sin hueso",
+        "Precio": 8.99,
+        "FechaRegistro": "2023-04-01T18:35:00.000Z",
+        "Stock_Cantidad": 30,
+        "IdUsrCreacion": 1,
+        "FechaCreacion": "2023-04-01T18:35:00.000Z"
+    },
+    {
+        "Id": 5,
+        "Nombre": "Pollo(und)",
+        "Descripcion": "Pollo fresco cortado en trozos",
+        "Precio": 5.25,
+        "FechaRegistro": "2023-04-01T18:35:00.000Z",
+        "Stock_Cantidad": 40,
+        "IdUsrCreacion": 1,
+        "FechaCreacion": "2023-04-01T18:35:00.000Z"
+    },
+    {
+        "Id": 6,
+        "Nombre": "Pescado(kg)",
+        "Descripcion": "Filetes de pescado fresco",
+        "Precio": 7.75,
+        "FechaRegistro": "2023-04-01T18:35:00.000Z",
+        "Stock_Cantidad": 20,
+        "IdUsrCreacion": 1,
+        "FechaCreacion": "2023-04-01T18:35:00.000Z"
+    },
+    {
+        "Id": 7,
+        "Nombre": "Pan integral(und)",
+        "Descripcion": "Pan integral recién horneado",
+        "Precio": 3.25,
+        "FechaRegistro": "2023-04-01T18:35:00.000Z",
+        "Stock_Cantidad": 80,
+        "IdUsrCreacion": 1,
+        "FechaCreacion": "2023-04-01T18:35:00.000Z"
+    },
+    {
+        "Id": 8,
+        "Nombre": "Huevos(und)",
+        "Descripcion": "Huevos frescos de gallinas libres de jaula",
+        "Precio": 2.50,
+        "FechaRegistro": "2023-04-01T18:35:00.000Z",
+        "Stock_Cantidad": 120,
+        "IdUsrCreacion": 1,
+        "FechaCreacion": "2023-04-01T18:35:00.000Z"
+    },
+    {
+        "Id": 9,
+        "Nombre": "Frijoles(kg)",
+        "Descripcion": "Frijoles negros cocidos y enlatados",
+        "Precio": 1.99,
+        "FechaRegistro": "2023-04-01T18:35:00.000Z",
+        "Stock_Cantidad": 150,
+        "IdUsrCreacion": 1,
+        "FechaCreacion": "2023-04-01T18:35:00.000Z"
+    },
+    {
+        "Id": 10,
+        "Nombre": "Arroz(kg)",
+        "Descripcion": "Arroz integral de grano largo",
+        "Precio": 4.75,
+        "FechaRegistro": "2023-04-01T18:35:00.000Z",
+        "Stock_Cantidad": 100,
+        "IdUsrCreacion": 1,
+        "FechaCreacion": "2023-04-01T18:35:00.000Z"
+    }
+]
---
+++ LicitacionesApi/WebAppUI/Scripts/View/InventarioProductos.js
@@ -0,0 +1,28 @@
+﻿function MostarInventario(Products) {
+
+    var arrColumns = [
+        { 'data': 'Id' },
+        { 'data': 'Nombre' },
+        { 'data': 'Descripcion' },
+        { 'data': 'Precio' },
+        { 'data': 'FechaRegistro' },
+        { 'data': 'Stock_Cantidad' },
+        { 'data': 'IdUsrCreacion' },
+        { 'data': 'FechaCreacion' }
+    ];
+
+    // Creamos la tabla con DataTables
+    var tablaProductos = $('#tblInventario').DataTable({
+        data: Products,
+        columns: arrColumns
+    });
+
+    // Añadimos el evento onclick a las filas de la tabla
+    $('#tblInventario tbody').off('click', 'tr').on('click', 'tr', function () {
+        // Obtenemos el ID del producto de la fila clickeada
+        var productId = $(this).find('td:eq(0)').text();
+
+        // Mostramos los detalles del producto en un modal o en otra sección de la página
+        mostrarDetallesProducto(productId);
+    }).css('cursor', 'pointer').attr('title', 'Click para ver detalles');
+}
