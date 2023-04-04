b5bfa0e9c85c497edada926f98732926ae1cecad
Denis Salazar <dsalazarc@ucenfotec.ac.cr>
2023-04-03 19:48:02 -0600

    Inventario productos btns

--- LicitacionesApi/WebAppUI/Scripts/View/InventarioProductos.js
+++ LicitacionesApi/WebAppUI/Scripts/View/InventarioProductos.js
@@ -1,31 +1,51 @@
-﻿function MostrarInventario(products) {
+﻿function MostrarInventario() {
     var tablaHTML = $('#tblInventario').DataTable();
     tablaHTML.clear().draw();
 
-    $.each(products, function (index, producto) {
+    $.each(Products, function (index, producto) {
         tablaHTML.row.add([
             producto.Id,
             producto.Nombre,
             producto.Descripcion,
             producto.Precio,
             producto.FechaRegistro,
-            producto.Stock_Cantidad
+            producto.Stock_Cantidad,
+            '<div class="btn-group" role="group">' +
+            '<button class="btn btn-sm btn-primary editar" data-toggle="tooltip" title="Editar"><i class="fas fa-pencil-alt"></i></button>' +
+            '<button class="btn btn-sm btn-danger eliminar" data-toggle="tooltip" title="Eliminar"><i class="fas fa-trash-alt"></i></button>' +
+            '</div>'
         ]).draw();
     });
 
-    $('#tblInventario tbody').on('click', 'tr', function () {
-        var tr = $(this);
-        var data = tablaHTML.row(tr).data();
-        var actionsC = new ActionsControl();
-        actionsC.BindFields("frmProducto", data);
-
-        $("#txtIdProducto").attr("ColumnDataName", "Id");
-        $("#txtProducto").attr("ColumnDataName", "Nombre");
-        $("#txtDescripcion").attr("ColumnDataName", "Descripcion");
-        $("#txtPrecio").attr("ColumnDataName", "Precio");
-        $("#txtFechaCreacion").attr("ColumnDataName", "FechaRegistro");
-        $("#txtCantidad").attr("ColumnDataName", "Stock_Cantidad");
-
+    $('#tblInventario thead').on('click', 'tr .crear', function () {
         $('#frmProducto').show();
+        $('#frmProducto #IdProduct').hide();
+        $('#frmProducto #FechaCreacion').hide();
+    });
+
+    $('#tblInventario tbody').on('click', 'tr .editar', function () {
+        $('#tblInventario tbody').on('click', 'tr .editar', function () {
+            var tr = $(this).closest('tr');
+
+            var data = tablaHTML.row(tr).data();
+            $('#txtIdProducto').val(data[0]);
+            $('#txtProducto').val(data[1]);
+            $('#txtDescripcion').val(data[2]);
+            $('#txtPrecio').val(data[3]);
+            $('#txtFechaCreacion').val(data[4]);
+            $('#txtCantidad').val(data[5]);
+
+            $('#frmProducto').show();
+        });
+    });
+
+    $('#tblInventario tbody').on('click', 'tr .eliminar', function () {
+        // lógica para eliminar el producto seleccionado
     }).css('cursor', 'pointer').attr('title', 'Click para ver detalles');
 }
+
+
+$('#frmProducto').on('click', '#btnCancelar', function () {
+    $('#frmProducto')[0].reset();
+    $('#frmProducto').hide();
+});
