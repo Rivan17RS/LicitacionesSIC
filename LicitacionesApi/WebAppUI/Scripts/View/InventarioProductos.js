da4bdb8b06bcb34dd33debbec594562e43e104fa
Denis Salazar <dsalazarc@ucenfotec.ac.cr>
2023-04-03 18:14:21 -0600

    Inventario productos

--- LicitacionesApi/WebAppUI/Scripts/View/InventarioProductos.js
+++ LicitacionesApi/WebAppUI/Scripts/View/InventarioProductos.js
@@ -1,28 +1,31 @@
-﻿function MostarInventario(Products) {
+﻿function MostrarInventario(products) {
+    var tablaHTML = $('#tblInventario').DataTable();
+    tablaHTML.clear().draw();
 
-    var arrColumns = [
-        { 'data': 'Id' },
-        { 'data': 'Nombre' },
-        { 'data': 'Descripcion' },
-        { 'data': 'Precio' },
-        { 'data': 'FechaRegistro' },
-        { 'data': 'Stock_Cantidad' },
-        { 'data': 'IdUsrCreacion' },
-        { 'data': 'FechaCreacion' }
-    ];
-
-    // Creamos la tabla con DataTables
-    var tablaProductos = $('#tblInventario').DataTable({
-        data: Products,
-        columns: arrColumns
+    $.each(products, function (index, producto) {
+        tablaHTML.row.add([
+            producto.Id,
+            producto.Nombre,
+            producto.Descripcion,
+            producto.Precio,
+            producto.FechaRegistro,
+            producto.Stock_Cantidad
+        ]).draw();
     });
 
-    // Añadimos el evento onclick a las filas de la tabla
-    $('#tblInventario tbody').off('click', 'tr').on('click', 'tr', function () {
-        // Obtenemos el ID del producto de la fila clickeada
-        var productId = $(this).find('td:eq(0)').text();
+    $('#tblInventario tbody').on('click', 'tr', function () {
+        var tr = $(this);
+        var data = tablaHTML.row(tr).data();
+        var actionsC = new ActionsControl();
+        actionsC.BindFields("frmProducto", data);
 
-        // Mostramos los detalles del producto en un modal o en otra sección de la página
-        mostrarDetallesProducto(productId);
+        $("#txtIdProducto").attr("ColumnDataName", "Id");
+        $("#txtProducto").attr("ColumnDataName", "Nombre");
+        $("#txtDescripcion").attr("ColumnDataName", "Descripcion");
+        $("#txtPrecio").attr("ColumnDataName", "Precio");
+        $("#txtFechaCreacion").attr("ColumnDataName", "FechaRegistro");
+        $("#txtCantidad").attr("ColumnDataName", "Stock_Cantidad");
+
+        $('#frmProducto').show();
     }).css('cursor', 'pointer').attr('title', 'Click para ver detalles');
 }
