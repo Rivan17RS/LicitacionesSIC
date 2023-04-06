de8b2faf6b376c50fc6390e7372b7f0bcf5048ed
Dns2690 <107601365+Dns2690@users.noreply.github.com>
2023-04-06 15:19:11 -0600

    entrega de productos

---
+++ LicitacionesApi/WebAppUI/Scripts/View/Entregas.js
@@ -0,0 +1,21 @@
+﻿var cameraOpen = false;
+
+function openCamera() {
+    if (cameraOpen) {
+        return; // Si la cámara ya está abierta, no hacer nada
+    }
+    cameraOpen = true; // Marcar la cámara como abierta
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then(function (stream) {
+            var video = document.createElement('video');
+            video.setAttribute('autoplay', '');
+            video.setAttribute('muted', '');
+            video.setAttribute('playsinline', '');
+            video.setAttribute('id', 'videoQR');
+            video.srcObject = stream;
+            document.getElementById("camera-container").appendChild(video);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
