4d68dfb7fb2d2ea2ac2a7739293e4220ec658208
Dns2690 <107601365+Dns2690@users.noreply.github.com>
2023-04-08 02:05:00 -0600

    confirmar registro js

---
+++ LicitacionesApi/WebAppUI/Scripts/View/ConfirmarRegistro.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $("#btn-confirmar-registro").on("click", function () {
+        var identificationNumber = $("#identificationNumberInput").val();
+        var otp = $("#otpInput").val();
+
+        var apiURL = "https://licitaciones-api.azurewebsites.net/api/Usuario/ValidarOtp?identificacion=" + identificationNumber + "&otp=" + otp;
+        $.ajax({
+            method: "POST",
+            url: apiURL,
+            contentType: "application/json;charset=utf-8",
+            data: {},
+            success: function (json) {
+                console.log(json);
+            }
+        });
+    });
+});
