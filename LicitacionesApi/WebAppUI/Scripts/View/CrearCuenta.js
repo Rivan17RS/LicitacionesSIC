f66d27ff328219f514148c6125d63d2725fc87cc
Zachary <zhernandezv@ucenfotec.ac.cr>
2023-04-08 07:32:15 -0600

    FInished crearcuenta

--- LicitacionesApi/WebAppUI/Scripts/View/CrearCuenta.js
+++ LicitacionesApi/WebAppUI/Scripts/View/CrearCuenta.js
@@ -1,4 +1,4 @@
-﻿$(document).ready(function () {
+﻿/*$(document).ready(function () {
     var userName = $("#userFirstName");
     var userLastName = $("#userSurnames");
     var userEmail = $("#userEmail");
@@ -42,4 +42,4 @@
             },
         });
     });
-});
+});*/
