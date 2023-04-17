da62a447ecbce62eb939c6198eda521f66c51402
irojass <irojass@ucenfotec.ac.cr>
2023-04-17 17:25:10 -0600

    Configuracion - View

---
+++ LicitacionesApi/WebApp/Controllers/PremiumController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AppLogic;
+using DTO;
+
+namespace WebApp.Controllers
+{
+    public class PremiumController : ApiController
+    {
+        PremiumManager pm = new PremiumManager();
+
+
+        
+    }
+}
---
+++ LicitacionesApi/WebAppUI/Controllers/ConfiguracionController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppUI.Controllers
+{
+    public class ConfiguracionController : Controller
+    {
+        // GET: Configuracion
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Configuracion()
+        {
+            return View();
+        }
+    }
+}
