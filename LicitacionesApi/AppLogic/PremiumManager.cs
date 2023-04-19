99623f9ceade6d4474aeb41e17558cfe24ae8f7b
irojass <irojass@ucenfotec.ac.cr>
2023-04-18 19:08:47 -0600

    Premium - Avance

--- LicitacionesApi/AppLogic/PremiumManager.cs
+++ LicitacionesApi/AppLogic/PremiumManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,44 @@ namespace AppLogic
 {
     public class PremiumManager
     {
-        public PremiumManager() 
+        PremiumCrudFactory pcf = new PremiumCrudFactory();
+        Validaciones v = new Validaciones();
+        public Subscripcion ObtenerPrecioMensual(int id)
         {
-            
+            PremiumCrudFactory pcf = new PremiumCrudFactory();
+            return pcf.RetrieveByID<Subscripcion>(id);
         }
+
+        public Response ActualizarMontoPremium(Subscripcion p)
+        {
+            Subscripcion prem = pcf.RetrieveByID<Subscripcion>(p.Id);
+ 
+            if (!v.ValidarNumero(Convert.ToDouble(p.PrecioMensual)))
+            {
+                return new Response("Error de entrada", "El valor debe ser mayor a 0", ResponseType.ERROR);
+            }
+    
+            prem.Nombre = p.Nombre;
+            prem.Descripcion = p.Descripcion;
+            prem.PrecioMensual = p.PrecioMensual;
+            prem.Estado = p.Estado;
+
+            try
+            {
+                pcf.Update(prem);
+
+            }
+
+            catch (SqlException ex)
+            {
+                if (ex.Number == 2601)
+                {
+                    return new Response("Error en la entrada", "No se pudo actualizar", ResponseType.ERROR);
+                }
+            }
+            return new Response("Success", "Membresía Actualizada exitosamente", ResponseType.SUCCESS);
+        }
+
 
     }
 }
---
+++ LicitacionesApi/DataAccess/CRUD/PremiumCrudFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.DAO;
+using DataAccess.MAPPER;
+using DTO;
+
+namespace DataAccess.CRUD
+{
+    public class PremiumCrudFactory : CrudFactory
+    {
+        private PremiumMapper mapper;
+
+        public PremiumMapper() : base()
+        {
+            mapper = new PremiumMapper();
+            dao = SqlDao.GetInstance();
+        }
+
+        public override T RetrieveByID<T>(int Id)
+        {
+            var data = dao.ExecuteQueryProcedureWithQuery(mapper.GetRetrieveByIDStatement(Id));
+            if (data.Count > 0)
+            {
+                var Obj = mapper.BuildObject(data[0]);
+                return (T)Convert.ChangeType(Obj, typeof(T));
+            }
+            return default(T);
+        }
+
+
+        public override void Update(BaseEntity entityDto)
+        {
+            var sqlOper = mapper.GetUpdateStatement(entityDto);
+            dao.ExecuteStoreProcedure(sqlOper);
+        }
+
+    }
+}
