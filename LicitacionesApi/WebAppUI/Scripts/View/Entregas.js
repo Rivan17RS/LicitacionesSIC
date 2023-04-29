747bc841eac7f4b67d3bec917bf164901ca2ae95
Dns2690 <107601365+Dns2690@users.noreply.github.com>
2023-04-28 19:52:17 -0600

    actualizacion del stock de productos

--- LicitacionesApi/WebAppUI/Scripts/View/Entregas.js
+++ LicitacionesApi/WebAppUI/Scripts/View/Entregas.js
@@ -1,6 +1,6 @@
 ﻿var cameraOpen = false;
 var video;
-
+var productos;
 function openCamera() {
     if (cameraOpen) {
         // Si la cámara ya está abierta, detener el stream y eliminar el elemento de video
@@ -40,7 +40,7 @@ $(document).ready(function () {
 
     $('#completar-reporte').on('click', function () {
         ActualizarLicitacion(estadoLicitacion);
-        ActualizarProductos();
+
     });
     });
 })
@@ -66,11 +66,12 @@ function mostrarDetallesLicitacion(idLicitacion) {
         url: "https://licitaciones-api.azurewebsites.net/api/DetalleLicitaciones/ObtenerDetalleLicitacionesId?IdLicitacion=" + idLicitacion,
         contentType: "application/json;charset=utf-8",
         success: function (data) {
+            productos = data;
             var promises = [];
             $.each(data, function (index, value) {
                 var promise = $.getJSON("https://licitaciones-api.azurewebsites.net/api/Producto/ObtenerProducto/" + value.Idproducto)
                     .then(function (product) {
-                        return '<tr><td><div class="form-check"><input class="form-check-input" type="checkbox" name="producto' + value.Idproducto + '"></div></td><td>' + (product ? product.Nombre : "Error al obtener producto") + '</td><td><input type="number" class="form-control form-control-sm" value="' + value.Cantidad + '" readonly></td><td><input type="number" class="form-control form-control-sm" name="producto' + value.Idproducto + '_cantidad"></td><td><select class="form-control form-control-sm" name="producto' + value.Idproducto + '_estado"><option value=""></option><option value="bien">Bien</option><option value="danado">Dañado</option><option value="incompleto">Incompleto</option><option value="faltante">Faltante</option></select></td></tr>';
+                        return '<tr><td><div class="form-check"><input class="form-check-input" type="checkbox" name="producto' + value.Idproducto + '"></div></td><td>' + (product ? product.Nombre : "Error al obtener producto") + '</td><td><input type="number" class="form-control form-control-sm" value="' + value.Cantidad + '" readonly></td><td><input  type="number" class="form-control form-control-sm"  name="producto' + value.Idproducto + '_cantidad" id="producto' + value.Idproducto + '"></td><td><select class="form-control form-control-sm" name="producto' + value.Idproducto + '_estado"><option value=""></option><option value="bien">Bien</option><option value="danado">Dañado</option><option value="incompleto">Incompleto</option><option value="faltante">Faltante</option></select></td></tr>';
                     });
                 promises.push(promise);
             });
@@ -113,11 +114,12 @@ function ActualizarLicitacion(estado) {
                     'Content-Type': "application/json"
                 },
                 type: 'POST',
-                url: "https://localhost:44369/api/Licitacion/ActualizarLicitacion",
+                url: "https://licitaciones-api.azurewebsites.net/api/Licitacion/ActualizarLicitacion",
                 contentType: "application/json",
                 data: JSON.stringify(lic),
                 success: function (response) {
-                    alert('Producto actualizado correctamente');
+                    alert('Licitación actualizada correctamente');
+                    ActualizarProductos(productos);
                 },
                 error: function (xhr, status, error) {
                     console.log(error);
@@ -129,5 +131,33 @@ function ActualizarLicitacion(estado) {
 }
 
 
+function ObtenerProducto(id) {
+    return $.ajax({
+        type: 'GET',
+        url: `https://licitaciones-api.azurewebsites.net/api/Producto/ObtenerProducto/${id}`,
+        contentType: "application/json"
+    });
+}
 
-
+async function ActualizarProductos(products) {
+    for (let product of products) {
+        try {
+            var productDetails = await ObtenerProducto(product.Idproducto);
+            productDetails.StockCantidad = $(`#producto${product.Idproducto}`).val();
+            await $.ajax({
+                headers: {
+                    'Accept': "application/json",
+                    'Content-Type': "application/json"
+                },
+                type: 'POST',
+                url: "https://licitaciones-api.azurewebsites.net/api/Producto/ActualizarProducto",
+                contentType: "application/json",
+                data: JSON.stringify(productDetails)
+                
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    location.reload();
+}
