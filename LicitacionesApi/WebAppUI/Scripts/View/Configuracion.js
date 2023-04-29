8e915d5a60227e3d0f41e45d12b9f6fcd4230415
irojass <irojass@ucenfotec.ac.cr>
2023-04-28 23:36:45 -0600

    Roles UI - Mostrar tabla

--- LicitacionesApi/WebAppUI/Scripts/View/Configuracion.js
+++ LicitacionesApi/WebAppUI/Scripts/View/Configuracion.js
@@ -15,15 +15,14 @@
             'data': 'Configuracion',
                 'render': function (data, type, full, meta) {
                     return '<div class="btn-group" role="group">' +
-                        '<button class="btn btn-sm btn-primary editar" data-toggle="tooltip" title="Editar"><i class="fas fa-pencil-alt"></i></button>' +
-                        '<button class="btn btn-sm btn-danger eliminar" data-toggle="tooltip" title="Eliminar"><i class="fas fa-trash-alt"></i></button>' +
+                        '<button type="button" class="btn btn-sm btn-primary editar" data-toggle="tooltip" title="Editar"><i class="fas fa-pencil-alt"></i></button>' +
+                        '<button type="button" class="btn btn-sm btn-danger eliminar" data-toggle="tooltip" title="Eliminar"><i class="fas fa-trash-alt"></i></button>' +
                         '</div>'
                 },
-            className: 'premiumConfig',
 
         },
 
-        $('#TblPremium').DataTable({
+        PremiumCargada = $('#TblPremium').DataTable({
             language: {
                 url: 'https://cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json'
             },
@@ -35,21 +34,175 @@
                     var jsonResult = { 'data': json };
                     return jsonResult.data;
 
-                 }
+                }
             },
             columns: arrayColumns
-            })
-  
+        });
+
+
+
+   
+        //To be reviewed
+
+        $('#TblPremium thead').on('click', 'tr .crear', function () {
+            $('#frmPremiumModal')[0].reset();
+            $('#frmPremiumModal #IdPremium').hide();
+   
+            $('#btnCrearPremium').off('click').on('click', function () {
+                CrearPremium();
+            });
+            $('#premiumModal').modal('show');
+        });
+    
+
+        $('#TblPremium tbody').on('click', 'tr .editar', function () {
+            $('#frmPremiumModal')[0].reset();
+            var tr = $(this).closest('tr');
+            var data = PremiumCargada.row(tr).data();
+            $('#txtIdPremium').val(data.Id);
+            $('#txtNombreP').val(data.Nombre);
+            $('#txtDescripcionP').val(data.Descripcion);
+            $('#txtPrecioP').val(data.PrecioMensual);
+            $('#txtEstadoP').val(data.Estado);
+            $('#frmPremiumModal #IdPremium').show();
+            $('#premiumModal').modal('show');
+            $('#btnGuardarPremium').off('click').on('click', function () {
+               ActualizarPremium();
+            });
+        });
+
+        $('#TblPremium tbody').on('click', 'tr .eliminar', function () {
+            var tr = $(this).closest('tr');
+            var data = PremiumCargada.row(tr).data();
+            var premiumId = data.Id;
+            EliminarPremium(premiumId);
+
+        }).css('cursor', 'pointer');
+
+
+        TablaRoles();
+
     }
-
-
 }
 
+
+
+function TablaRoles() {
+
+    var arrayColumns = [];
+    arrayColumns[0] = { 'data': 'Id' };
+    arrayColumns[1] = { 'data': 'Nombre' };
+    arrayColumns[2] = { 'data': 'Estado' };
+    arrayColumns[3] = {
+                        'data': 'Configuracion',
+                        'render': function (data, type, full, meta) {
+                            return '<div class="btn-group" role="group">' +
+                                '<button class="btn btn-sm btn-primary editar" data-toggle="tooltip" title="Editar"><i class="fas fa-pencil-alt"></i></button>' +
+                                '<button class="btn btn-sm btn-danger eliminar" data-toggle="tooltip" title="Eliminar"><i class="fas fa-trash-alt"></i></button>' +
+                                '</div>'
+                        },
+            
+    },
+
+    RolesCargado = $('#TblRoles').DataTable({
+            language: {
+                url: 'https://cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json'
+            },
+                ajax: {
+                    method: "GET",
+                    url: "https://licitaciones-api.azurewebsites.net/api/Role/ObtenerRoles",
+                    contentType: "application/json;charset=utf-8",
+                    dataSrc: function (json) {
+                        var jsonResult = { 'data': json };
+                        return jsonResult.data;
+                    }
+                },
+                columns: arrayColumns
+    })
+
+
+
+
+ }
+
+
+
+function CrearPremium() {
+    var ap = {};
+    ap.Nombre = $('#txtNombreP').val();
+    ap.Descripcion = $('#txtDescripcionP').val();
+    ap.PrecioMensual = $('#txtPrecioP').val();
+    ap.Estado = $('#txtEstadoP').val()
+    ap.IdUsrCreacion = 4;
+    if (confirm("¿Está seguro que desea crear este plan?")) {
+        $.ajax({
+            type: 'POST',
+            url: "https://licitaciones-api.azurewebsites.net/api/Premium/CrearPremium",
+            contentType: "application/json",
+            data: JSON.stringify(ap),
+            success: function (response) {
+                alert('Plan actualizado correctamente');
+            },
+            error: function (xhr, status, error) {
+                console.log(error);
+                alert('Error, no se pudo crear');
+
+            }
+        });
+    }
+}
+
+
+
+function ActualizarPremium() {
+    var ap = {};
+    ap.Id = $('#txtIdPremium').val();
+    ap.Nombre = $('#txtNombreP').val();
+    ap.Descripcion = $('#txtDescripcionP').val();
+    ap.PrecioMensual = $('#txtPrecioP').val();
+    ap.Estado = $('#txtEstadoP').val()
+    ap.IdUsrActualizacion = 41;
+    if (confirm("¿Está seguro que desea actualizar este plan?")) {
+        $.ajax({
+            type: 'POST',
+            url: "https://licitaciones-api.azurewebsites.net/api/Premium/ActualizarPremium",
+            contentType: "application/json",
+            data: JSON.stringify(ap),
+            success: function (response) {
+                alert('Plan actualizado correctamente');
+            },
+            error: function (xhr, status, error) {
+                console.log(error);
+                alert('Error, no se pudo actualizar');
+
+            }
+        });
+    }
+}
+
+
+function EliminarPremium(Id) {
+    if (confirm("¿Está seguro que desea eliminar este plan?")) {
+        $.ajax({
+            headers: {
+                'Accept': "application/json",
+                'Content-Type': "application/json"
+            },
+            type: 'POST',
+            url: "https://licitaciones-api.azurewebsites.net/api/Premium/EliminarPremium/" + Id,
+            success: function (response) {
+                alert('Plan eliminado');
+            }
+        });
+    }
+}
+
+
+
 $(document).ready(function () {
-    var view = new TablaPremium(); //Crea una instancia de la funcion principal
-    view.InitView(); //Llama metodo inicializador de propiedades
+ 
+
+    var view = new TablaPremium();
+    view.InitView();
+
 });
-
-
-
-
